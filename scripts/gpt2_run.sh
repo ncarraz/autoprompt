8605cff3cd06282fdf4511e5674ebf5ecd93a722
ncarraz <carraznathanael@live.fr>
2022-06-30 15:11:10 +0200

    fix run scripts

--- scripts/gpt2_run.sh
+++ scripts/gpt2_run.sh
@@ -5,7 +5,7 @@ do
 python -m autoprompt.create_trigger \
     --train data/filtered_original/$REL/train.jsonl \
     --dev data/filtered_original/$REL/dev.jsonl \
-    --template "$2" \
+    --template '{sub_label} [T] [T] [T] [T] [T] [P]. </s>' \
     --num-cand 10 \
     --accumulation-steps 1 \
     --model-name $1 \
