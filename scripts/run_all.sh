dbcafab0a55de31e35a473b63388484bdcb35d7d
ncarraz <carraznathanael@live.fr>
2022-06-30 13:51:01 +0200

    add run scripts

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -7,12 +7,12 @@ done
 
 for LM in "roberta-base" "roberta-large" "allenai/longformer-base-4096" "allenai/longformer-large-4096" "distilroberta-base" "facebook/bart-base" "facebook/bart-large"  
 do
-scripts/run.sh $LM '<s> {sub_label} [T] [T] [T] [T] [T] [P].</s>'
+bash scripts/run.sh $LM '<s> {sub_label} [T] [T] [T] [T] [T] [P].</s>'
 done
 
 for LM in "t5-small" "t5-base" "t5-large"  
 do
-scripts/run.sh $LM '{sub_label} [T] [T] [T] [T] [T] [P]. </s>'
+bash scripts/run.sh $LM '{sub_label} [T] [T] [T] [T] [T] [P]. </s>'
 done
 
-scripts/run.sh gpt2 '{sub_label} [T] [T] [T] [T] [T] [P].' 
+bash scripts/run.sh gpt2 '{sub_label} [T] [T] [T] [T] [T] [P].' 
