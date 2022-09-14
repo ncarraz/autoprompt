e18acd4f579f722a109abeda2db1cdfd26e66185
ncarraz <carraznathanael@live.fr>
2022-09-14 14:54:07 +0200

    remove unused files

---
+++ run_mix_autoprompt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for REL in $(ls data/filtered_original/)
+do
+python -m autoprompt.create_trigger \
+    --train data/filtered_original/$REL/train.jsonl \
+    --dev data/filtered_original/$REL/dev.jsonl \
+    --template ' {sub_label} [T] [T] [T] [T] [T] [P].' \
+    --num-cand 10 \
+    --accumulation-steps 1 \
+    --model-name $1 \
+    --eval-model-name $2 \
+    --bsz 20 \
+    --eval-size 100 \
+    --iters 1000 \
+    --label-field 'obj_label' \
+    --tokenize-labels \
+    --filter \
+    --print-lama 
+done
